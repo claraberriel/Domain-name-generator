8862cbc7991e596294b15f0d3254222e7c838d59
claraberriel <berrielclara@gmail.com>
2021-04-21 13:52:14 +0000

    .

--- src/app.js
+++ src/app.js
@@ -6,11 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+
+function domainGenerator() {
+
+
   //write your code here
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
   let domain = [".com", ".uy"];
+
+  let domainGenerated = [];
 
   for (var i = 0; i < pronoun.length; i++)
   {
@@ -19,11 +25,21 @@ window.onload = function() {
         for (var k = 0; k < noun.length; k++)
         {
             for (var l = 0; l < domain.length; l++){
-                console.log(pronoun[i]+adj[j]+noun[k]+domain[l]);
+                domainGenerated.push(pronoun[i] + adj[j] + noun[k] + domain[l]);
+
+               // console.log(pronoun[i]+adj[j]+noun[k]+domain[l]);
             }
         }
     }
   }
+  return domainGenerated;   
+}
+  let domainText = document.getElementById("domain");
+  let btnGenerator = document.getElementById("btnGenerator");
+
+  btnGenerator.addEventListener("click", function(){
+      domainText.innerHTML = 
+  });
 
   console.log("Hello Rigo from the console!");
 };
